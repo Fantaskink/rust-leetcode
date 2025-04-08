dc6ee62df53d2792fe220c3d52b8203727130008
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2025-04-08 19:31:11 +0200

    Solve "Remove Element"

---
+++ src/problems/remove_element.rs
@@ -0,0 +1,22 @@
+pub struct Solution;
+
+#[allow(dead_code)]
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        nums.retain(|x| *x != val);
+        nums.len() as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_remove_element() {
+        let mut vec1 = vec![3,2,2,3];
+        assert_eq!(2, Solution::remove_element(&mut vec1, 3));
+        assert_eq!(vec![2,2], vec1);
+        //assert_eq!(5, Solution::remove_element(&mut vec![0,1,2,2,3,0,4,2], 2));
+    }
+}
